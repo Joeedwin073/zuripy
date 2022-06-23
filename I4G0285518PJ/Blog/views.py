bd2784a801c8c1d0135e680b4310a505893cddff
Joe Edwin <Joeedwin073@gmail.com>
2022-06-23 22:01:39 +0000

    CRUD commit for django project

--- I4G0285518PJ/Blog/views.py
+++ I4G0285518PJ/Blog/views.py
@@ -1,3 +1,38 @@
+from django import views
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from python_assignments.zuripy.I4G0285518PJ.Blog.models import Post
+
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+
+    fields = "__all__"
+
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+    fields = "__all__"
+
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+
+    fields = "__all__"
+
+    success_url  = reverse_lazy("blog:all")
+
